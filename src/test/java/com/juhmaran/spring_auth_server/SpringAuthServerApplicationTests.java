b653aa505788f54a762467a1419d1dd55b946aaa
JuhMaran <julianemaran@gmail.com>
2026-03-29 07:57:36 -0300

    new project

---
+++ src/test/java/com/juhmaran/spring_auth_server/SpringAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhmaran.spring_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
